dc98c4d53625b4c3016194093bef4ab328107db9
ArshadAliNasir <165890957+ArshadAliNasir@users.noreply.github.com>
2024-05-02 14:44:34 +0500

    hi

--- script.js
+++ script.js
@@ -43,5 +43,40 @@ $(document).ready(function(){
     $("body").removeClass("drawer-open");
 
     });   
+    $(".plus").click(function(){
+      // $(".plus-dropdown").slideDown("slow");
+    });
+  $(".plus").click(function(){
+  //     console.log("click")
+  // $(".minus").show()
+  // $(".plus").hide()
+  });
 
+
+
+
+  $(".fa-solid").click(function(){
+  //   var list =$(this).siblings(".plus");
+  // list.slideToggle()
+  // $(this).slideDown(".plus");
+  $(this).parents(".items").next(".plus-dropdown").slideToggle();
+  $(this).toggleClass("fa-plus") 
+  $(this).toggleClass("fa-minus")  
+});
+
+
+
+
+
+
+//   $(".plus-dropdowm").click(function(){
+//     console.log("click")
+//     $(this).hide();
+
+// });
+
+  // $(".time").click(function(){
+  //     console.log("click")
+  // $(".dropdown-content").hide()
+  // });
 });
