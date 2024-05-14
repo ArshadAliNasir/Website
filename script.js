1d117b755b20ffbb2b4ba89b0820b23e863c1c2c
ArshadAliNasir <165890957+ArshadAliNasir@users.noreply.github.com>
2024-05-14 14:42:18 +0500

    y

--- script.js
+++ script.js
@@ -1,34 +1,8 @@
 $(document).ready(function(){
-  // $(".Bars").click(function(){
-  //     console.log("click")
-  // $(".dropdown-content").show()
-  // });
 
-  // $(".time").click(function(){
-  //     console.log("click")
-  // $(".dropdown-content").hide()
-  // });
+   
 
-     $(".Bars").click(function(){
-      console.log("click")
-  $(".drop-menu-content").addClass("drop-menu-content-show")
-  $("body").css("overflow", "hidden");
-  $(".overlay").fadeIn();
-
-  });
-
-    $(".time").click(function(){
-      console.log("click")
-  $(".drop-menu-content").removeClass("drop-menu-content-show");
-  $("body").css("overflow", "auto");
-  $(".overlay").fadeOut();
-
-  });
-
-  // $(".container").click(function(){
-  //     console.log("click")
-  // $(".dropdown-content").removeClass("dropdown-content-show")
-  // });
+  
   $(".Bars").click(function(){
     console.log("click")
        $(".dropdown-content").addClass("dropdown-content-show")
